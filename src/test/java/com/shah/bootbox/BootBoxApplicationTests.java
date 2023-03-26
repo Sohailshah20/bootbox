05168a9cc56a423f0ea4227aee6694beeb5018b1
Sohailshah20 <contact.sohailali@gmail.com>
2023-03-26 15:58:04 +0530

    intial project

---
+++ src/test/java/com/shah/bootbox/BootBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shah.bootbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
